65318c97ef852e67a1e8ba5724a7f5fa85c5cdd9
lhw414 <dlgusdn0414@snu.ac.kr>
2023-02-11 22:13:09 +0900

    2023.02.11(2)

--- DP(no30 - no50)/no40.py
+++ DP(no30 - no50)/no40.py
@@ -4,10 +4,10 @@ import sys
 
 N = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
-ans = [[0 for _ in range(N)], [0 for _ in range(N)]]
-
-for i in range(N):
+ans = arr[:]
+for i in range(1, N):
     for j in range(i):
-        if arr[i] > arr[j] and ans[i] < ans[j]:
-            ans[i] = ans[j]
-    ans[i] += 1
+        if arr[j] < arr[i]:
+            ans[i] = max(ans[i], ans[j] + arr[i])
+
+print(max(ans))
