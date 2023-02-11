7a0276644d082d07e51d14c422c85bfaffa1ade7
lhw414 <dlgusdn0414@snu.ac.kr>
2023-02-11 21:37:32 +0900

    2023.02.11

---
+++ DP(no30 - no50)/no39.py
@@ -0,0 +1,15 @@
+# BJ 11053
+
+import sys
+
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+ans = [0 for _ in range(N)]
+
+for i in range(N):
+    for j in range(i):
+        if arr[i] > arr[j] and ans[i] < ans[j]:
+            ans[i] = ans[j]
+    ans[i] += 1
+
+print(max(ans))
---
+++ DP(no30 - no50)/no40.py
@@ -0,0 +1,13 @@
+# BJ 11055
+
+import sys
+
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+ans = [[0 for _ in range(N)], [0 for _ in range(N)]]
+
+for i in range(N):
+    for j in range(i):
+        if arr[i] > arr[j] and ans[i] < ans[j]:
+            ans[i] = ans[j]
+    ans[i] += 1
