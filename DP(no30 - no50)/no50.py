fbe350065a3e70f90036756d25cc45d87e547c7d
lhw414 <dlgusdn0414@snu.ac.kr>
2023-02-16 23:28:32 +0900

    2023.02.16

--- DP(no30 - no50)/no50.py
+++ DP(no30 - no50)/no50.py
@@ -4,6 +4,7 @@ import sys
 
 N = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
+
 for i in range(1, N):
     for j in range(0, i):
         arr[i] = max(arr[i], arr[i-j-1] + arr[j])
